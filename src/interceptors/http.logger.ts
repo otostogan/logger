7f78ef86dabb4cf9105699e99620138eed03e367
Alexandru Tostogan <otostogan2020@gmail.com>
2025-02-06 09:31:04 +0200

    httpInterceptor

---
+++ src/interceptors/http.logger.ts
@@ -0,0 +1,55 @@
+import {
+	CallHandler,
+	ExecutionContext,
+	Inject,
+	Injectable,
+	NestInterceptor,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { PublisherService } from '../logger/logger.service';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class HTTPLoggerInterceptor implements NestInterceptor {
+	constructor(
+		private readonly _publisher: PublisherService,
+		@Inject('EXCLUDE_ROUTE_LOGGING')
+		private readonly excludeRoutes: string[],
+	) {}
+
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const request = context.switchToHttp().getRequest();
+		const { method, url, body, params, query } = request;
+		const requestId = uuidv4();
+		if (!this.excludeRoutes.includes(url)) {
+			this._publisher.info(
+				`TRIGGERED: ${method} '${url}' body:${JSON.stringify({
+					body,
+				})}, params: ${JSON.stringify({ params })}, query: ${JSON.stringify(
+					{
+						query,
+					},
+				)}, requestID: ${requestId}`,
+			);
+		}
+
+		return next.handle().pipe(
+			tap(() => {
+				if (!this.excludeRoutes.includes(url)) {
+					this._publisher.log(
+						`Completed ${method} ${url}, requestID: ${requestId}`,
+					);
+				}
+			}),
+			catchError((error) => {
+				this._publisher.critical(
+					`ENDPOINT ERROR ${method} '${url}' ${JSON.stringify(
+						error,
+					)}, requestID: ${requestId}`,
+				);
+				return throwError(() => error);
+			}),
+		);
+	}
+}
